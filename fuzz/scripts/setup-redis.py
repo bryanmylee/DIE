d5fb8b777e088bd8d1f63faa6b36aa30e66d0600
Bryan Lee <bryanmylee@gmail.com>
2021-06-16 18:18:10 +0800

    feat: use args for redis setup script

--- fuzz/scripts/setup-redis.py
+++ fuzz/scripts/setup-redis.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import argparse
 import os
 import subprocess
 #export REDIS_URL=redis://localhost:9000
@@ -11,9 +12,14 @@ if "ssh-tunneling" in out.decode("utf-8"):
 
 print("This script makes ssh-tunneling between your redis-server and this machine.")
 
-host = input("redis-server HOST : ")
-sshport = input("redis-server SSH PORT : ")
-port = input("redis-server REDIS PORT : ")
-login = input("redis-server USER ID : ")
+def parse_args():
+    p = argparse.ArgumentParser()
+    p.add_argument('master_host')
+    p.add_argument('master_ssh_port')
+    p.add_argument('master_redis_port')
+    p.add_argument('master_user_id')
+    return p.parse_args()
 
-os.system(f"tmux new-session -s ssh-tunneling -d 'ssh -L 9000:{host}:{port} {login}@{host} -p {sshport}'")
+args = parse_args()
+tunnel_cmd = f"'ssh -L 9000:{args.master_host}:{args.master_redis_port} {args.master_user_id}@{args.master_host} -p {args.master_ssh_port}'";
+os.system(f"tmux new-session -s ssh-tunneling -d {tunnel_cmd}")

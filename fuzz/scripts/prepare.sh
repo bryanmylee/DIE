292406bebeb0eab0c428e33b3a9b92232ae69fae
Bryan Lee <bryanmylee@gmail.com>
2021-06-17 23:47:12 +0800

    fix: non-zero exit code

--- fuzz/scripts/prepare.sh
+++ fuzz/scripts/prepare.sh
@@ -4,4 +4,5 @@ echo core >/proc/sys/kernel/core_pattern
 
 cd /sys/devices/system/cpu
 [ -f cpu0/cpufreq/scaling_governor ] && \
-  echo performance | tee cpu*/cpufreq/scaling_governor
+  echo performance | tee cpu*/cpufreq/scaling_governor || \
+  true
